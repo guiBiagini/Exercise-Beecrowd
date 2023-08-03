58213d634514c162f6b68e3f19324f09e3a3955f
Gui Biagini <guilhermebiagini@dcc.ufmg.br>
2023-08-02 21:41:10 -0300

    1019

---
+++ 1019/src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        
+        int valor = scanner.nextInt();
+        int horas = 0;
+        int minutos = 0;
+        int segundos = 0;
+
+        if (valor >= 60) {
+            minutos = valor / 60;
+            segundos = valor % 60;
+            if (minutos >= 60) {
+                horas = minutos / 60;
+                minutos = minutos % 60;
+            }
+        } else
+            segundos = valor;
+
+        System.out.println(horas + ":" + minutos + ":" + segundos);
+
+    }
+}
