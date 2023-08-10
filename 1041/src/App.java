d43bc20a764afd4311ec62024852e63169975304
Gui Biagini <guilhermebiagini@dcc.ufmg.br>
2023-08-10 11:54:09 -0300

    1041

---
+++ 1041/src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+        scanner.close();
+
+        if (x == 0 && y == 0)
+            System.out.println("Origem");
+        else if (x == 0)
+            System.out.println("Eixo Y");
+        else if (y == 0)
+            System.out.println("Eixo X");
+        else if (x > 0 && y > 0)
+            System.out.println("Q1");
+        else if (x < 0 && y > 0)
+            System.out.println("Q2");
+        else if (x > 0 && y < 0)
+            System.out.println("Q4");
+        else
+            System.out.println("Q3");
+
+    }
+}
